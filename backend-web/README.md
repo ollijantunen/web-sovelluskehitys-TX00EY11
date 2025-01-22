427cfd5f8bbcf68ccefc6780820809f065a79c65
ollijantunen <32096381+ollijantunen@users.noreply.github.com>
2025-01-23 01:28:48 +0200

    added tehtavat-route, 404 answer and some test requests

--- backend-web/README.md
+++ backend-web/README.md
@@ -1,6 +1,8 @@
 #### Web-sovelluskehitys
 ##### Backend -tehtävä, viikko 1
 
+---
+
 ###### Tehtävänanto
 [https://github.com/mattpe/hyte-web-dev/blob/main/02-node-express.md#week-assignment-1---getting-started-with-nodejs]
 
@@ -14,22 +16,23 @@ Week assignment 1 - Getting started with node.js
 - [x] readme.md
 - [x] nodemon for running the dev environment (use local npm package and start with npm script)
 - [x] git repo: include .gitignore and set a remote repo in Github, create and checkout a new branch git checkout -b week1
-**.gitignore on projektin juuressa** https://github.com/ollijantunen/web-sovelluskehitys-TX00EY11/blob/week1/.gitignore
+_.gitignore on projektin juuressa_ https://github.com/ollijantunen/web-sovelluskehitys-TX00EY11/blob/week1/.gitignore
 
 ##### Implement a (dummy) REST API including following resources/endpoints:
-- [ ] read some data from server (send response in json format)
-- [ ] send some data to server
+- [x] read some data from server (send response in json format)
+- [x] send some data to server
 - [ ] delete data (just a dummy functionality, test error response too)
 - [ ] modify something (just a dummy functionality, test error response too)
-- [ ] send 404 response for non-existing resources
+- [x] send 404 response for non-existing resources
 
 ##### Adapt HTTP standards:
 - [ ] use appropriate HTTP methods for requests
 - [ ] set correct status codes and Content-Type headers to responses
 
+##### Test
 - [ ] Test with Postman or similar
 
-- [ ] Commit your changes (git add . & git commit -m "describe you changes here") and push your week1 branch to the remote repo in Github (git push -u origin week1)
+- [x] Commit your changes (git add . & git commit -m "describe you changes here") and push your week1 branch to the remote repo in Github (git push -u origin week1)
 
 Extra: Add more funtionalities to your REST API
 
--- backend-web/src/index.js
+++ backend-web/src/index.js
@@ -6,23 +6,22 @@ const port = 3000;
 // Staattinen html-sivusto tarjoillaan palvelimen juuressa
 app.use(express.static('public'));
 
-// middleware, joka lukee json-datan POST-pyyntöjen rungosta/hyötykuormasta(body)
+// Middleware, joka lukee json-datan POST-pyyntöjen rungosta/hyötykuormasta(body)
 app.use(express.json());
 
-// REST-apin resurssit tarjoillaan muualla, tässä /api/-polun alla
+// REST-apin resurssit tarjoillaan muualla kuin juuressa, tässä /api/-polun alla
 app.get('/api', (req, res) => {
-  console.log('get-pyyntö juureen');
-  console.log(req.url);
+  console.log('get-pyyntö reittiin', req.url);
 
-  res.send('Welcome to my REST API! Ke 22.1.');
+  res.send('Welcome to my REST API!');
 });
 
 // Syötteen lukeminen reittiparametreista (route parameter(=params))
 app.get('/api/sum/:num1/:num2', (req, res) => {
-  console.log(req.params);
+  console.log('req.params', req.params);
 
   // testataan, että onko käyttäjän syötteistä jompikumpi ei-numero
-  // esim. parseInt (etsii numeron 2def-> 2) / Number (desimaalit käy, js ei ole int)
+  // esim. parseInt (etsii numeron 2def-> 2) / Number (desimaalit käy, koska js:ssä ei ole int)
   // virhetilakoodi ja viesti json-muodossa
   const num1 = parseInt(req.params.num1);
   const num2 = Number(req.params.num2);
@@ -43,7 +42,7 @@ app.get('/api/sum/:num1/:num2', (req, res) => {
 
 // Syötteen lukeminen kyselyparametreista (query parameter(=params))
 app.get('/api/sum/', (req, res) => {
-  console.log(req.query);
+  console.log('req.query', req.query);
   const num1 = parseInt(req.query.num1);
   const num2 = parseInt(req.query.num2);
 
@@ -64,6 +63,36 @@ app.post('/api/moro', (req, res) => {
 
 // TODO: lisää oma reitti ja toiminnallisuus
 
+// GET-pyyntö tehtäviin, vastaus JSON-muodossa
+app.get('/api/tehtavat', (req, res) => {
+  console.log('GET tehtavat', req.url);
+
+  const tehtavalista = [
+    {id: '1', tehtava: 'Tehtäväkuvaus 1'},
+    {id: '2', tehtava: 'Tehtäväkuvaus 2'},
+    {id: '3', tehtava: 'Tehtäväkuvaus 3'},
+  ];
+  console.log(tehtavalista);
+
+  res.json(tehtavalista);
+});
+
+// POST-pyyntö tehtäviin, vastaus JSON-muodossa
+app.post('/api/tehtavat', (req, res) => {
+  console.log(req.body);
+  res.status(201);
+  res.json({vastaus: "Tehtäväkuvaus lisätty", runko: req.body});
+  });
+
+// Kaikkien olemattomien resurssien vastaus 404
+// Asteriski toimii, koska tämä kohta on viimeisenä reittinä tiedostossa
+app.all("*", (req, res) => {
+  console.log("Olematon resurssi.");
+  res.status(404);
+res.send("Reittiä ei löydy.");
+})
+
+// Serveri kuuntelee porttia osoitteessa yhteydenottojen varalta
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
