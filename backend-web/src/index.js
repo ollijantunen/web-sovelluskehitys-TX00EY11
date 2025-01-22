075cdddd62980875de81be93add4d261a9506987
Olli Jantunen <32096381+ollijantunen@users.noreply.github.com>
2025-01-22 15:13:30 +0200

    class activity: public-static and more routes

--- backend-web/src/index.js
+++ backend-web/src/index.js
@@ -3,17 +3,66 @@ const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my REST API!');
+// Staattinen html-sivusto tarjoillaan palvelimen juuressa
+app.use(express.static('public'));
+
+// middleware, joka lukee json-datan POST-pyyntöjen rungosta/hyötykuormasta(body)
+app.use(express.json());
+
+// REST-apin resurssit tarjoillaan muualla, tässä /api/-polun alla
+app.get('/api', (req, res) => {
+  console.log('get-pyyntö juureen');
+  console.log(req.url);
+
+  res.send('Welcome to my REST API! Ke 22.1.');
 });
 
-app.get('/moro', (req, res) => {
-  res.send('Moro!');
+// Syötteen lukeminen reittiparametreista (route parameter(=params))
+app.get('/api/sum/:num1/:num2', (req, res) => {
+  console.log(req.params);
+
+  // testataan, että onko käyttäjän syötteistä jompikumpi ei-numero
+  // esim. parseInt (etsii numeron 2def-> 2) / Number (desimaalit käy, js ei ole int)
+  // virhetilakoodi ja viesti json-muodossa
+  const num1 = parseInt(req.params.num1);
+  const num2 = Number(req.params.num2);
+  if (isNaN(num1) || isNaN(num2)) {
+    res.status(400);
+    res.json({
+      error: 'Molempien parametrien tulee olla numero',
+    });
+    return;
+  }
+
+  res.json({
+    num1,
+    num2,
+    sum: num1 + num2,
+  });
 });
 
-app.post('/moro', (req, res) => {
-  res.send('Moro! Post version');
+// Syötteen lukeminen kyselyparametreista (query parameter(=params))
+app.get('/api/sum/', (req, res) => {
+  console.log(req.query);
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+
+  res.json({
+    num1,
+    num2,
+    sum: num1 + num2,
+  });
 });
+
+// POST-pyynnön käsittely ja datan lukeminen rungosta
+app.post('/api/moro', (req, res) => {
+  console.log(req.body);
+  res.status(200);
+  res.json({reply: 'No moro ' + req.body.sender});
+  // res.send('Moro! Post version');
+});
+
+// TODO: lisää oma reitti ja toiminnallisuus
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
